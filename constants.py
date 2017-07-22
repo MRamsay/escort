add87b61fb9b2132cb18fcbd8140957b5c8fe20b
MRamsay <jasperarlington@gmail.com>
2017-07-21 23:54:27 -0500

    Put most string literals into constants

---
+++ constants.py
@@ -0,0 +1,24 @@
+FPS = 120
+WINDOW_WIDTH = 1680
+WINDOW_HEIGHT = 1050
+GAME_TITLE = "Escort"
+WHITE = [255, 255, 255]
+RED = [255, 0, 0]
+GREEN = [0, 255, 0]
+BLUE = [0, 0, 255]
+BLACK = [0, 0, 0]
+SPEED = .5  # pixels per second
+SAVED_SHIPS_REQUIRED = [5, 20, 100]
+
+IMAGE_PATH = "images/"
+
+FONT_NAME = "RENT_0.ttf"
+BACKGROUND_NAME = "background.png"
+TRANSPARENT_BACKGROUND_NAME = "background_transparent.png"
+
+SOUND_TWISTED = "Twisted.mp3"
+SOUND_LASER = "laser.wav"
+SOUND_SNAP = "snap.wav"
+SOUND_EXPLOSION = "explosion.aiff"
+
+TITLE = "/ESCORT\\"
